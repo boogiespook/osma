8aeb38b7b4e9b1ef5d34ee5a2f36144b26c70c37
Chris Jenkins <cjenkins@redhat.com>
2019-03-13 08:08:11 +0000

    Some questions removed from insert

--- tmp.php
+++ tmp.php
@@ -15,12 +15,12 @@ $res = mysqli_query($db, $q1);
 $row = mysqli_fetch_assoc($res);
 $data['user'] = $row['name'];
 
-$fields = array('user','client','rhEmail','country','lob','consume','collaborate','createOS','policy','question11','question12','question13','question14','question15','question21','question22','question23','question24','question25','question26','question31','question32','question33','question41','question42','question43','question44','question51','question52','question53','area1_comments','area2_comments','area3_comments','area4_comments','area5_comments','hash');
+$fields = array('user','client','rhEmail','country','lob','consume','collaborate','createOS','policy','question11','question12','question13','question14','question15','question21','question22','question23','question24','question25','question31','question32','question33','question41','question42','question43','question51','question52','question53','area1_comments','area2_comments','area3_comments','area4_comments','area5_comments','hash');
 foreach ($fields as $field) {
 	$$field = mysqli_real_escape_string($db, $data[$field]);
 }
 #var_dump($fields);
-$qq = "INSERT INTO data (" . implode(',', $fields).") VALUES ('$user','$client','$rhEmail','$country','$lob',$consume,$collaborate,$createOS,$policy,$question11,$question12,$question13,$question14,$question15,$question21,$question22,$question23,$question24,$question25,$question26,$question31,$question32,$question33,$question41,$question42,$question43,$question44,$question51,$question52,$question53,'$area1_comments','$area2_comments','$area3_comments','$area4_comments','$area5_comments','$hash')";
+$qq = "INSERT INTO data (" . implode(',', $fields).") VALUES ('$user','$client','$rhEmail','$country','$lob',$consume,$collaborate,$createOS,$policy,$question11,$question12,$question13,$question14,$question15,$question21,$question22,$question23,$question24,$question25,$question31,$question32,$question33,$question41,$question42,$question43,$question51,$question52,$question53,'$area1_comments','$area2_comments','$area3_comments','$area4_comments','$area5_comments','$hash')";
 #print $qq;
 $result = mysqli_query($db, $qq);
 
